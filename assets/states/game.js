0c43e1be794fe96b2c0b23a27524324ff307ec0b
Carlitoshsh <caragomezher@unal.edu.co>
2017-05-27 22:44:34 -0500

    doing map

--- assets/states/game.js
+++ assets/states/game.js
@@ -1,7 +1,27 @@
+var nivel;
+var conjuntoPatrones;
+var pantalla;
+var capa;
+
 var game =  {
+  preload:function(){
+    // Se cargan el fondo y el laberinto a resolver
+    //project.load.tilemap('level1', 'assets/gameAssets/level1.json', null, Phaser.Tilemap.TILED_JSON);
+    project.load.image('fondo', 'assets/img/project/sky.jpg');
+    project.load.tilemap('textura', 'assets/map/desert.json', null, Phaser.Tilemap.TILED_JSON);
+    project.load.image('Dungeon', 'assets/map/Dungeon.png');
+
+  },
+
   create: function(){
     project.stage.backgroundColor = '#454645';
-
+    pantalla = project.add.tileSprite(0, 0, 320, 480, 'fondo');
+    nivel = project.add.tilemap('textura');
+	// El archivo anterior depende de la imagen que solicita tiles, puede verificarse en los assets.
+    nivel.addTilesetImage('Dungeon');
+    // Dentro del json, este es el nombre de ...
+    capa = nivel.createLayer('Ground');
+    capa.resizeWorld();
     //  By default Phaser only starts 2 pointers (enough for 2 fingers at once)
 
     //  addPointer tells Phaser to add another pointer to Input, so here we are basically saying "allow up to 6 pointers + the mouse"
